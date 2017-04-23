8b4598f813ed4687f28bcf8d969c6d8e8a949246
yugecin <yugecin@users.noreply.github.com>
2017-04-23 14:17:43 +0200

    cleanup world2hud src

--- cleoplugins/world2hud/opcodes.cpp
+++ cleoplugins/world2hud/opcodes.cpp
@@ -6,12 +6,10 @@ BOOL InitOpcodes()
 	return
 		CLEO_RegisterOpcode(0x0C27, &Op_0C27) &&
 		CLEO_RegisterOpcode(0x0C28, &Op_0C28);
-
 }
 
 RwV3D TransformWorldXYZ(RwV3D world) {
-	// partly based on
-	https://github.com/multitheftauto/mtasa-blue/blob/9b1fbaf639657a446790469aa6711e11be0d3eee/MTA10/core/CGraphics.cpp#L498
+	// partly based on https://github.com/multitheftauto/mtasa-blue/blob/9b1fbaf639657a446790469aa6711e11be0d3eee/MTA10/core/CGraphics.cpp#L498
 
 	CMATRIX *cameraViewMatrix = (CMATRIX*)(0xB6FA2C);
 
@@ -27,10 +25,7 @@ RwV3D TransformWorldXYZ(RwV3D world) {
 	return screen;
 }
 
-// 0C27: transform world_xyz 1@ 2@ 3@ to hud_xy 4@ 5@
-// {$O 0C27=5,transform world_xyz %1d% %2d% %3d% to hud_xy %4d% %5d%}
-OpcodeResult WINAPI Op_0C27(CScriptThread *thread)
-{
+OpcodeResult TransformPointFor0C270C28(CScriptThread *thread, int width, int height) {
 	RwV3D world;
 	world.x = CLEO_GetFloatOpcodeParam(thread);
 	world.y = CLEO_GetFloatOpcodeParam(thread);
@@ -38,26 +33,23 @@ OpcodeResult WINAPI Op_0C27(CScriptThread *thread)
 
 	RwV3D screen = TransformWorldXYZ(world);
 
-	CLEO_SetFloatOpcodeParam(thread, screen.x * 640.0);
-	CLEO_SetFloatOpcodeParam(thread, screen.y * 480.0);
+	CLEO_SetFloatOpcodeParam(thread, screen.x * width);
+	CLEO_SetFloatOpcodeParam(thread, screen.y * height);
 	return OR_CONTINUE;
+}
+
+// 0C27: transform world_xyz 1@ 2@ 3@ to hud_xy 4@ 5@
+// {$O 0C27=5,transform world_xyz %1d% %2d% %3d% to hud_xy %4d% %5d%}
+OpcodeResult WINAPI Op_0C27(CScriptThread *thread)
+{
+	return TransformPointFor0C270C28(thread, 640, 480);
 }
 
 // 0C28: transform world_xyz 1@ 2@ 3@ to screen_xy 4@ 5@
 // {$O 0C28=5,transform world_xyz %1d% %2d% %3d% to screen_xy %4d% %5d%}
 OpcodeResult WINAPI Op_0C28(CScriptThread *thread)
 {
-	RwV3D world;
-	world.x = CLEO_GetFloatOpcodeParam(thread);
-	world.y = CLEO_GetFloatOpcodeParam(thread);
-	world.z = CLEO_GetFloatOpcodeParam(thread);
-
-	RwV3D screen = TransformWorldXYZ(world);
-
 	DWORD *displayWidth = (DWORD*)(0xC17044);
 	DWORD *displayHeight = (DWORD*)(0xC17048);
-
-	CLEO_SetFloatOpcodeParam(thread, screen.x * (*displayWidth));
-	CLEO_SetFloatOpcodeParam(thread, screen.y * (*displayHeight));
-	return OR_CONTINUE;
+	return TransformPointFor0C270C28(thread, (*displayWidth), (*displayHeight));
 }
