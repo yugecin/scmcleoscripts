a09fe968e5697462bd3906739d66edc1e2884f32
yugecin <yugecin@users.noreply.github.com>
2017-08-15 21:10:48 +0200

    fix disappearing health textdraw

--- cleoplugins/plhudmod/theme_standard.cpp
+++ cleoplugins/plhudmod/theme_standard.cpp
@@ -126,7 +126,7 @@ void damagepatchhandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, i
 BOOL setupTextdraws()
 {
 	setupTD(PLTD_FUEL, 0x44078000, 0x43C48000, 0, 0, NULL);
-	setupTD(PLTD_DAMAGE, 0x44044000, 0x43CB0000, 0, 0, &damagepatchhandler);
+	setupTD(PLTD_DAMAGE, 0x44044000, 0x43CB0000, 0x44044000, 0x43CB0148, &damagepatchhandler);
 	setupTD(PLTD_STATUSBARBOX, 0x43A00000, 0x43D60000, 0, 0, NULL);
 	setupTD(PLTD_FUELDMGBOX, 0x4403C000, 0x43C48000, 0, 0, NULL);
 	setupTD(PLTD_FUELPRICE, 0x44000000, 0x42C80000, 0, 0, NULL);
