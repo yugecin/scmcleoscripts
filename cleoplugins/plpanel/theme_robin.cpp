e3dafd97af58c0ec06f966c444010cfd6a32b281
yugecin <yugecin@users.noreply.github.com>
2017-08-17 23:49:57 +0200

    fix compiler warnings

--- cleoplugins/plpanel/theme_robin.cpp
+++ cleoplugins/plpanel/theme_robin.cpp
@@ -353,7 +353,7 @@ void __cdecl drawbars()
 
 	float BARPOSITIONX = 547.0f * WIDTHMP;
 	int BARWIDTH = (int) (50.0f * WIDTHMP);
-	float BARHEIGHT = (9.2f * HEIGHTMP);
+	int BARHEIGHT = (int) (9.2f * HEIGHTMP);
 	float DBARPOSITIONY = (9.0f * HEIGHTMP);
 
 	float barpositiony = (383.0f * HEIGHTMP);
