e58e7b3caa7a336c45102d66beb180655ec94cfb
yugecin <yugecin@users.noreply.github.com>
2017-08-19 00:05:06 +0200

    avoid unneeded buffers in common handlers

--- cleoplugins/plpanel/commonhandlers.cpp
+++ cleoplugins/plpanel/commonhandlers.cpp
@@ -8,18 +8,14 @@ void headinghandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, int r
 	if (!INCAR) {
 		return;
 	}
-#define HEADINGSTRLEN (3 + 1 + 3 + 1 + 3 + 1 + 5 + 1 + 3 + 1 + 3 + 1 + 3 + 1)
-	char headingstring[HEADINGSTRLEN];
 	int n[7];
 	for (int i = 0; i < 7; i++) {
 		n[i] = (360 - gamedata.carheading) - 3 + i;
 		if (n[i] < 1) n[i] += 360;
 		if (n[i] > 360) n[i] -= 360;
 	}
-	sprintf(headingstring, "%03d %03d %03d [%03d] %03d %03d %03d", n[0], n[1], n[2], n[3], n[4], n[5], n[6]);
-	memcpy(samptd->szText, headingstring, HEADINGSTRLEN);
-	memcpy(samptd->szString, headingstring, HEADINGSTRLEN);
-#undef HEADINGSTRLEN
+	sprintf(samptd->szText, "%03d %03d %03d [%03d] %03d %03d %03d", n[0], n[1], n[2], n[3], n[4], n[5], n[6]);
+	memcpy(samptd->szString, samptd->szText, 50);
 }
 
 void removehandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, int reason)
@@ -46,10 +42,8 @@ void carspeedtdhandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, in
 	if (!INCAR) {
 		return;
 	}
-	char carspeedstring[10];
-	sprintf(carspeedstring, "%d KPH", GROUNDSPEED(gamedata.carspeed));
-	memcpy(&samptd->szText[13], carspeedstring, 10);
-	memcpy(&samptd->szString[13], carspeedstring, 10);
+	sprintf(&samptd->szText[13], "%d KPH", GROUNDSPEED(gamedata.carspeed));
+	memcpy(&samptd->szString[13], &samptd->szText[13], 10);
 }
 
 void progressbarpatchhandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, int reason)
@@ -83,10 +77,8 @@ void damagepatchhandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, i
 	if (!INCAR) {
 		return;
 	}
-	char str[12];
-	sprintf(str, "  Health");
-	memcpy(samptd->szText, str, 12);
-	memcpy(samptd->szString, str, 12);
+	sprintf(samptd->szText, "  Health");
+	memcpy(samptd->szString, samptd->szText, 12);
 }
 
 void destnearesthandler(struct SPLHXTEXTDRAW *hxtd, struct stTextdraw *samptd, int reason, char *destnearstr)
