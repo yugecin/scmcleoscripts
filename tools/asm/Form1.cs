70a28a358ff09235dab2c48acef51213affd3830
yugecin <yugecin@users.noreply.github.com>
2017-08-27 00:26:22 +0200

    asmtool: support vars with offsets, use caching for label offsets

--- tools/asm/Form1.cs
+++ tools/asm/Form1.cs
@@ -4,8 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace asm {
@@ -27,6 +27,8 @@ namespace asm {
 			string[] instr = new string[8];
 			int instrc;
 			int instrs = 0;
+			DATA.lvars.Clear();
+			DATA.cache.Clear();
 			SortedDictionary<int, A> stuff = new SortedDictionary<int, A>();
 			sb.Append(":ENTRY").AppendLine();
 			sb.Append("hex").AppendLine();
@@ -60,16 +62,16 @@ namespace asm {
 					patchinstr(instr, instrs);
 				}
 				for (int i = 1; i <= instrc - 4; i++) {
-					if (instr[i + 1] != "00" || instr[i + 2] != "00" || instr[i + 3] != "ee") {
+					if (instr[i + 3] != "ee") {
 						continue;
 					}
 					for (; si < i; si++) {
 						sb.Append(instr[si]).Append(' ');
 						instrs++;
 					}
-					stuff.Add(instrs, new DATA(instr[i]));
+					stuff.Add(instrs, new DATA(instr[i + 2], (int.Parse(instr[i + 1], NumberStyles.HexNumber) << 8) | int.Parse(instr[i], NumberStyles.HexNumber)));
 					sb.AppendLine();
-					sb.Append("00 00 00 00 // DATA").Append(instr[i]).AppendLine();
+					sb.Append("00 00 00 00 // DATA").Append(instr[i + 2]).AppendLine();
 					instrs += 4;
 					i += 3;
 					si += 3;
@@ -178,14 +180,15 @@ skiptocomment:
 				if (line.Trim().Length == 0) {
 					continue;
 				}
-				int i = line.IndexOf(';');
+				string l = new Regex("_var(..)").Replace(line, "0xEE${1}0000");
+				int i = l.IndexOf(';');
 				if (i == -1) {
-					sb.Append(line.Replace("_var", "0xEE0000")).AppendLine();
+					sb.Append(l).AppendLine();
 					continue;
 				}
-				string txt = line.Substring(0, i).Trim();
+				string txt = l.Substring(0, i).Trim();
 				if (txt.Length > 0) {
-					sb.Append(txt.Replace("_var", "0xEE0000")).AppendLine();
+					sb.Append(txt).AppendLine();
 				}
 			}
 			textBox1.Text = sb.ToString();
@@ -210,12 +213,39 @@ skiptocomment:
 
 		public class DATA : A {
 			public string n;
-			public DATA(string n) {
+			public int offset;
+			public static HashSet<int> lvars = new HashSet<int>();
+			public static Dictionary<string, int> cache = new Dictionary<string, int>();
+			public DATA(string n, int offset) {
 				this.n = n;
+				this.offset = offset;
 			}
 			void A.dostuff(StringBuilder sb) {
-				sb.Append("0AC6: 2@ = label @DATA").Append(n).Append(" offset").AppendLine();
-				sb.Append("0A8C: write_memory 1@ size 4 value 2@ vp 0").AppendLine();
+				int var = 2;
+				if (cache.TryGetValue(n, out var)) {
+					if (offset != 0) {
+						goto dooffset;
+					}
+					goto skip;
+				}
+				for (int i = 3; i < 33; i++) {
+					if (lvars.Contains(i)) {
+						continue;
+					}
+					lvars.Add(i);
+					cache.Add(n, i);
+					var = i;
+					break;
+				}
+				sb.Append("0AC6: ").Append(var).Append("@ = label @DATA").Append(n).Append(" offset").AppendLine();
+dooffset:
+				if (offset != 0) {
+					sb.Append("0085: 2@ = ").Append(var).Append("@ // (int)").AppendLine();
+					sb.Append("000A: 2@ += 0x").Append(offset).AppendLine();
+					var = 2;
+				}
+skip:
+				sb.Append("0A8C: write_memory 1@ size 4 value ").Append(var).Append("@ vp 0").AppendLine();
 			}
 		}
 	}
