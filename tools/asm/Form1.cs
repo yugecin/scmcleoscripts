e4f5fc2f419e75f95e0da1a017f503013b74c589
yugecin <yugecin@users.noreply.github.com>
2017-08-26 21:42:44 +0200

    asm tool: better adjustment of offsets & data seg

--- tools/asm/Form1.cs
+++ tools/asm/Form1.cs
@@ -27,7 +27,7 @@ namespace asm {
 			string[] instr = new string[8];
 			int instrc;
 			int instrs = 0;
-			int labels = 0;
+			SortedDictionary<int, A> stuff = new SortedDictionary<int, A>();
 			sb.Append(":ENTRY").AppendLine();
 			sb.Append("hex").AppendLine();
 			foreach (var line in lines) {
@@ -53,22 +53,38 @@ namespace asm {
 						(comment.StartsWith("call") || comment.StartsWith("jmp") || comment.StartsWith("je ") || comment.StartsWith("jne "))) {
 					si = 1;
 					sb.Append(instr[0]).AppendLine();
-					sb.Append("end").AppendLine();
-					sb.Append(":OFF").Append(labels++).AppendLine();
-					sb.Append("hex").AppendLine();
 					instrs++;
+					stuff.Add(instrs, new JMPCALL());
 					int idx = comment.IndexOf(' ') + 1;
 					comment = comment.Substring(0, idx) + "0x" + realaddr(instr);
 					patchinstr(instr, instrs);
 				}
-				comment = comment.Trim();
+				for (int i = 1; i <= instrc - 4; i++) {
+					if (instr[i + 1] != "00" || instr[i + 2] != "00" || instr[i + 3] != "ee") {
+						continue;
+					}
+					for (; si < i; si++) {
+						sb.Append(instr[si]).Append(' ');
+						instrs++;
+					}
+					stuff.Add(instrs, new DATA(instr[i]));
+					sb.AppendLine();
+					sb.Append("00 00 00 00 // DATA").Append(instr[i]).AppendLine();
+					instrs += 4;
+					i += 3;
+					si += 3;
+					if (si == instrc - 1) {
+						goto skiptocomment;
+					}
+				}
 				for (; si < instrc; si++) {
 					sb.Append(instr[si]).Append(' ');
 					instrs++;
 				}
 				sb.AppendLine();
+skiptocomment:
 				if (checkBox1.Checked) {
-					sb.Append("// ").Append(comment).AppendLine();
+					sb.Append("// ").Append(comment.Trim()).AppendLine();
 				}
 			}
 			sb.Append("end").AppendLine();
@@ -87,13 +103,17 @@ namespace asm {
 			sb2.Append("0A8C: write_memory 0x").Append(i2hs(int.Parse(textBox2.Text, NumberStyles.HexNumber) - 1)).Append(" size 1 value 0xE9 vp 0").AppendLine();
 			sb2.Append("0A8C: write_memory 0x").Append(textBox2.Text).Append(" size 4 value 1@ vp 0").AppendLine();
 			sb2.AppendLine();
-			for (int i = 0; i < labels; i++) {
-				sb2.Append("0AC6: 1@ = label @OFF").Append(i).Append(" offset").AppendLine();
-				sb2.Append("0A8D: 2@ = read_memory 1@ size 4 vp 0").AppendLine();
-				sb2.Append("0062: 2@ -= 0@  // (int)").AppendLine();
-				sb2.Append("0A8C: write_memory 1@ size 4 value 2@ vp 0").AppendLine();
+			sb2.Append("0AC6: 1@ = label @ENTRY offset").AppendLine();
+			sb2.AppendLine();
+			int offset = 0;
+			foreach (KeyValuePair<int, A> s in stuff) {
+				int diff = s.Key - offset;
+				sb2.Append("000A: 1@ += ").Append(diff).AppendLine();
+				offset += diff;
+				s.Value.dostuff(sb2);
 			}
-			sb2.AppendLine().Append("0A93: end_custom_thread").AppendLine().AppendLine();
+			sb2.AppendLine();
+			sb2.Append("0A93: end_custom_thread").AppendLine().AppendLine();
 			sb2.Append(":MAIN_LOOP").AppendLine();
 			sb2.Append("0001: wait 250").AppendLine();
 			sb2.Append("0002: jump @MAIN_LOOP").AppendLine();
@@ -160,10 +180,13 @@ namespace asm {
 				}
 				int i = line.IndexOf(';');
 				if (i == -1) {
-					sb.Append(line).AppendLine();
+					sb.Append(line.Replace("_var", "0xEE0000")).AppendLine();
 					continue;
 				}
-				sb.Append(line.Substring(0, i).Trim()).AppendLine();
+				string txt = line.Substring(0, i).Trim();
+				if (txt.Length > 0) {
+					sb.Append(txt.Replace("_var", "0xEE0000")).AppendLine();
+				}
 			}
 			textBox1.Text = sb.ToString();
 			Clipboard.SetText(textBox1.Text);
@@ -171,6 +194,29 @@ namespace asm {
 
 		private void button3_Click(object sender, EventArgs e) {
 			textBox1.Text = "";
+		}
+
+		public interface A {
+			void dostuff(StringBuilder sb);
+		}
+
+		public class JMPCALL : A {
+			void A.dostuff(StringBuilder sb) {
+				sb.Append("0A8D: 2@ = read_memory 1@ size 4 vp 0").AppendLine();
+				sb.Append("0062: 2@ -= 0@  // (int)").AppendLine();
+				sb.Append("0A8C: write_memory 1@ size 4 value 2@ vp 0").AppendLine();
+			}
+		}
+
+		public class DATA : A {
+			public string n;
+			public DATA(string n) {
+				this.n = n;
+			}
+			void A.dostuff(StringBuilder sb) {
+				sb.Append("0AC6: 2@ = label @DATA").Append(n).Append(" offset").AppendLine();
+				sb.Append("0A8C: write_memory 1@ size 4 value 2@ vp 0").AppendLine();
+			}
 		}
 	}
 }
